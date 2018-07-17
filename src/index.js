3cb27b35fc681fe88671ed2f35769d0cde766896
Margarita Obraztsova <marharyta.obraztsova@gmail.com>
2018-07-17 16:02:08 +0300

    Add internal state for the app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './css/main.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { coworkings: [] };
+  }
+  componentDidMount() {
+    fetch('https://api.myjson.com/bins/11sr9q', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return <div> Hello, world! </div>;
   }
